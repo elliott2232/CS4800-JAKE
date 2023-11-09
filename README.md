99cb37f82d5d6781af2336364194ea61193c6292
jperrello <jperrello8@gmail.com>
2023-11-08 19:57:23 -0800

    updated comments

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS4800-JAKE
-This is our project for CS4800 our group name is JAKE (J)oey (A)llen (K)ennedy (E)lliott
--- searchapp.py
+++ searchapp.py
@@ -5,7 +5,7 @@ from main import search_articles, connect_to_cluster
 
 app = Flask(__name__)
 
-app.static_folder = 'static'
+app.static_folder = 'static' #flask only will display static files, so images and css pages needed to go in static folder
 
 html_template = """
 <!DOCTYPE html>
@@ -114,7 +114,7 @@ html_template = """
 """
 
 @app.route('/')
-def index():
+def index(): #render the html page from string above^^
     return render_template_string(html_template)
 
 @app.route('/search', methods=['POST'])
@@ -123,7 +123,7 @@ def search():
     client = connect_to_cluster("mongodb+srv://Allan123:School123@cluster0.gqdysfd.mongodb.net/Articles?retryWrites=true&w=majority")
     results = search_articles(client, search_query, "Computer Science")
 
-    return jsonify({'results': results})
+    return jsonify({'results': results})# get results in json format
 
 if __name__ == '__main__':
     app.run(debug=True)
